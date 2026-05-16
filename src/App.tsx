6c6b49514f0eed8906b1839da01a7dc1fb8ba5e4
Tim Otto <timothydavidotto@gmail.com>
2026-05-16 11:33:03 +0100

    pass userId to Progress screen

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ export default function App() {
           onClose={() => { setActiveMobility(null); setScreen('home'); }}
         />
       )}
-      {screen === 'progress' && <Progress history={history} onRefetch={refetch} />}
+      {screen === 'progress' && <Progress history={history} onRefetch={refetch} userId={user.id} />}
 
       {screen !== 'workout' && screen !== 'mobility' && (
         <BottomNav
